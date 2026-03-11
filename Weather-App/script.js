6ec938e92fc36e6cdb0a78209f57e45c8b7e1989
Variant <praisekaye13@gmail.com>
2026-03-11 23:50:33 +0100

    Start of weather App

---
+++ Weather-App/script.js
@@ -0,0 +1,12 @@
+
+const apiKey = '6834c4cbfa39e474b884977c6cb5169d';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=berlin';
+const searchBtn = document.getElementById('search-btn');
+const cityInput = document.getElementById('city-input');
+const weatherInfo = document.getElementById('weather-info');
+const cityName = document.getElementById('cityName');
+const temperature = document.getElementById('temperature');
+const humidity = document.getElementById('humidity');
+const wind = document.getElementById('wind');
+
+
